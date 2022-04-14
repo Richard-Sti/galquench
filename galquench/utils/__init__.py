708ed05e78f091c51f3b0641061eb4f429705b7a
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-14 18:12:19 +0200

    Add function to read supplementary catalogs

--- galquench/utils/__init__.py
+++ galquench/utils/__init__.py
@@ -1,1 +1,2 @@
 from .illustris_parse_groupcats import (loadHalos, loadSubhalos)
+from .illustris_load import (subhalos_to_array, read_supplementary)
--- galquench/utils/illustris_load.py
+++ galquench/utils/illustris_load.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy
+import h5py
 
 
 def subhalos_to_array(subhalos, legend):
@@ -23,3 +24,38 @@ def subhalos_to_array(subhalos, legend):
     for key, val in out.items():
         arr[key] = val
     return arr
+
+
+def read_supplementary(file, subfindID, keys=None, skip_keys=None, snapshot_number=None):
+    data = h5py.File(file, "r")
+    # Get the snapshot
+    if snapshot_number is not None:
+        snapshot = "Snapshot_{}".format(snapshot_number)
+        if snapshot not in data.keys():
+            raise ValueError("Invalid snapshot number `{}`".format(snapshot_number))
+        # Get just this snapshot
+        data = data[snapshot]
+    # Check if a specific key(s) given
+    if keys is None:
+        keys = list(data.keys())
+        keys.remove(subfindID)
+    elif isinstance(keys, str):
+        keys = [keys]
+    else:
+        for key in keys:
+            if key not in data.keys():
+                raise ValueError("Invalid key `{}`.".format(key))
+
+    # If any skip keys, remove them
+    if skip_keys is not None:
+        if isinstance(skip_keys, str):
+            skip_keys = [skip_keys]
+        for skip_key in skip_keys:
+            if skip_key in keys:
+                keys.remove(skip_key)
+
+    # Put into a dictionary and return
+    out = {"subfindID": numpy.asarray(data[subfindID]).astype(int)}
+    for key in keys:
+        out.update({key: numpy.asarray(data[key])})
+    return out
