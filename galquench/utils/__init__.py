25e9c7ade406e9eafbef64f58ff535a7ff7e878a
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-15 00:12:58 +0200

    add imports to __init__

--- galquench/utils/__init__.py
+++ galquench/utils/__init__.py
@@ -1,2 +1,4 @@
 from .illustris_parse_groupcats import (loadHalos, loadSubhalos)
-from .illustris_load import (subhalos_to_array, read_supplementary)
+from .illustris_load import (
+    read_supplementary, unpack_catalog_columns, match_subfind,
+    merge_subhalos_with_supplementary)
